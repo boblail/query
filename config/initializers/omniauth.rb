9d6b593983dd069e2d2f43f8eae0a85a7014adc4
Bob Lail <bob.lail@cph.org>
2015-01-25 17:50:35 -0600

    [fix] Enabled authorization to work for those whose membership in an organization is concealed (5m)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret
+  provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret,
+    scope: "user"
 end
